1597477302b774f1ac38a6686c92dcddc2a5865c
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2022-06-13 17:53:47 +0430

    adding files to the github

---
+++ todo-app/todo-app.js
@@ -0,0 +1,56 @@
+const todos = [{
+    text: 'Order cat food',
+    completed: false
+}, {
+    text: 'Clean kitchen',
+    completed: true
+}, {
+    text: 'Buy food',
+    completed: true
+}, {
+    text: 'Do work',
+    completed: false
+}, {
+    text: 'Exercise',
+    completed: true
+}]
+
+const filters = {
+    searchText: ''
+}
+const renderTodos = (todos, filters) => {
+    const filteredTodos = todos.filter(todo => todo.text.toLowerCase().includes(filters.searchText.toLowerCase()))
+
+    document.querySelector('#todos').innerHTML = ''
+
+    const incompleteTodos = filteredTodos.filter(function (todo) {
+        return !todo.completed
+    })
+    
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${incompleteTodos.length} todos left`
+    document.querySelector('#todos').appendChild(summary)
+
+    filteredTodos.forEach(todo => {
+        const todoEl = document.createElement('p')
+        todoEl.textContent = todo.text
+        document.querySelector('#todos').appendChild(todoEl)
+    })
+}
+
+renderTodos(todos, filters)
+
+document.querySelector('#search-text').addEventListener('input', (e) => {
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
+})
+
+document.querySelector('#new-todo').addEventListener('submit', e => {
+    e.preventDefault()
+    todos.push({
+        text: e.target.elements.text.value,
+        completed: false
+    })
+    renderTodos(todos, filters)
+    e.target.elements.text.value = ''
+})
