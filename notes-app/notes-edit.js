7d3def910b5e6fb2bcb88c1fdcbd418121ee32a4
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2022-06-17 15:31:01 +0430

    section 8.13

--- notes-app/notes-edit.js
+++ notes-app/notes-edit.js
@@ -2,24 +2,22 @@ const titleElement = document.querySelector('#note-title')
 const bodyElement = document.querySelector('#note-body')
 const removeElement = document.querySelector('#remove-note')
 const noteId = location.hash.substring(1)
-const notes = getSavedNotes()
+let notes = getSavedNotes()
 
-const note = notes.find(note => note.id === noteId)
+let note = notes.find(note => note.id === noteId)
 if (!note) {
     location.assign('/index.html')
 }
 
-const noteIndex = notes.findIndex(note => note.id === noteId)
-
 titleElement.value = note.title
 bodyElement.value = note.body
 
-titleElement.addEventListener('change', e => {
+titleElement.addEventListener('input', e => {
     note.title = e.target.value
     saveNotes(notes)
 })
 
-bodyElement.addEventListener('change', e => {
+bodyElement.addEventListener('input', e => {
     note.body = e.target.value
     saveNotes(notes)
 })
@@ -28,4 +26,17 @@ removeElement.addEventListener('click', () => {
     removeNote(note.id)
     saveNotes(notes)
     location.assign('/index.html')
+})
+
+window.addEventListener('storage', (e) => {
+    if (e.key === 'notes') {
+        notes = JSON.parse(e.newValue)
+        note = notes.find(note => note.id === noteId)
+        if (!note) {
+            location.assign('/index.html')
+        }
+
+        titleElement.value = note.title
+        bodyElement.value = note.body
+    }
 })
