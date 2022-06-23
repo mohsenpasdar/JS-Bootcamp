6e85cbb42dfbeb0bcb4880ff723b0dc45ffc23f2
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2022-06-23 14:16:16 +0430

    sectionn 9.8

--- advanced/conditional-operator.js
+++ advanced/conditional-operator.js
@@ -3,3 +3,5 @@ const team = ['ali', 'mohsen', 'rtajj', 'fdsf', 'dsds']
 const message = team.length <= 4 ? `team size: ${team.length}` : 'too many people on ypur team'
 
 console.log(message);
+
+console.log(typeof []);
