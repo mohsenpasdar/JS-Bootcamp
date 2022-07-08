ba84a91e4d599f8069b80cd172908a67e67316f4
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2022-07-08 15:04:52 +0430

    section 11.2

--- hangman/app.js
+++ hangman/app.js
@@ -13,3 +13,14 @@ window.addEventListener('keypress', (e) => {
     }
 })
 
+const request = new XMLHttpRequest()
+
+request.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4) {
+        const data = JSON.parse(e.target.responseText)
+        console.log(data.puzzle);
+    }
+})
+
+request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.send()
