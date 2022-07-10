d423cd2c4627ad4efd207edd770231c30ce4165f
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2022-07-10 12:38:02 +0430

    section 11.5

--- hangman/app.js
+++ hangman/app.js
@@ -13,16 +13,10 @@ window.addEventListener('keypress', (e) => {
     }
 })
 
-const request = new XMLHttpRequest()
-
-request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText)
-        console.log(data.puzzle);
-    } else if (e.target.readyState === 4) {
-        console.log('An error has taken place');
+getPuzzle((error, puzzle) => {
+    if (error) {
+        console.log(`Error: ${error}`);
+    } else {
+        console.log(puzzle);
     }
 })
-
-request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
-request.send()
